1e08559fff50a38fd05587b2f9a2488460f620fe
Chris McKenzie <kristopolous@yahoo.com>
2013-01-28 13:15:13 -0800

    Added Glicks code + converter

---
+++ import/glick-importer.js
@@ -0,0 +1,102 @@
+#!/usr/local/bin/node
+var xml2js = require('xml2js'),
+    cheerio = require('cheerio'),
+    fs = require('fs'),
+    output = [],
+    UID = 0,
+    CIDMap = {},
+    CID = 0,
+    stack = [];
+
+var parser = new xml2js.Parser();
+
+function Link(dest) {
+  if(!CIDMap[dest]) {
+    CIDMap[dest] = CID++;
+  }
+  return CIDMap[dest];
+}
+
+function parse(txt) {
+  var mtxt = txt,
+      content = [];
+  [
+    [/.dummy/, ''],
+    [/(^.*)<a/g, '<span>$1</span><a'],
+    [/a>(.*)$/, 'a><span>$1</span>'],
+    [/^([^<].*)$/, '<span>$1</span>']
+  ].forEach(function(what) {
+    mtxt = mtxt.replace(what[0], what[1]);
+  });
+
+  var $ = cheerio.load(mtxt);
+  $("*").each(function(i, el) {
+    var txt = $(this).text();
+    if(txt.length) {
+      if (el.name == 'a') {
+        if(el.attribs.href) {
+          var id = Link(el.attribs.href.slice(1));
+          content.push({
+            type: 'link', 
+            cid: id, 
+            data: txt
+          });
+        } else {
+          var id = Link(el.attribs.name);
+          content.push({
+            type: 'dest', 
+            cid: id, 
+            data: txt
+          });
+        }
+      } else {
+        content.push({
+          type: 'text',
+          data: txt
+        });
+      }
+    }
+  });
+  return content;
+}
+
+function recurse(what, type) {
+
+  stack.push(UID++);
+
+  if(what.$) {
+    var payload = {
+      parent: stack.slice(-2)[0],
+      uid: stack.slice(-1)[0],
+      type: type,
+      content: parse(what.$.id.replace(/\s+/g, ' '))
+    };
+    if(what.$.transition) {
+      payload.label = what.$.transition;
+    }
+    output.push(payload);
+  }
+  [
+    ['Node', 'Category'],
+    ['Item', 'Description']
+  ].forEach(function(tuple) {
+    if(what[tuple[0]]) {
+      what[tuple[0]].forEach(function(item) {
+        recurse(item, tuple[1]);
+      });
+    }
+  });
+
+  stack.pop();
+}
+
+if(process.argv.length > 2) {
+  fs.readFile(process.argv[2], 'utf-8', function(err, data) {
+    parser.parseString(data, function(err, res) {
+      recurse(res.Node, 'Category');
+    });
+    console.log(JSON.stringify(output));
+  });
+} else {
+  console.log("I need an xml file to convert");
+}
--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-var slice = Array.prototype.slice;
-
-function inheritAndAdd(type, obj) {
-  return _.extend(
-    {}, (
-      _.isFunction(type.prototype.defaults) ?
-        type.prototype.defaults() :
-        type.prototype.defaults
-    ),
-    obj
-  );
-}
-
 var register = (function(){
   var UID = 0;
   return function (what) {
@@ -19,53 +6,10 @@ var register = (function(){
   };
 })();
         
-// see http://en.wikipedia.org/wiki/Decorator_pattern
-function decorate(initial, decorator) {
-  return function() {
-    return decorator.call(this, initial.apply(this, slice.call(arguments)));
-  }
-}
-
-// The Steinhaus-Johnson-Trotter algorithm
-function permute(array) {
-  // Identity
-  if(array.length == 0) {
-    return [];
-  }
-  var ret = [array],
-      len = array.length,
-      modlen = len - 1,
-      counter = 0,
-      mover = array[len - 1];
-
-  _.each(permute(array.slice(0, -1)), function(which) {
-    if(++ counter % 2) {
-      for(var ix = modlen - 1; ix > (0 - 1); ix += -1) {
-        ret.push(
-          which.slice(0, ix).concat(
-            [mover],
-            which.slice(ix)
-          )
-        );
-      }
-    } else {
-      for(var ix = 0; ix < (modlen + 1); ix += 1) {
-        ret.push(
-          which.slice(0, ix).concat(
-            [mover],
-            which.slice(ix)
-          )
-        );
-      }
-    }
-  });
-
-  return ret;
-}
-
-var v = {}, u = 0;
+// inner spans for view
+var m = {}, u = 0;
 (function ($) {
-  var Shape = v.Shape = Backbone.Model.extend({
+  var Shape = m.Shape = Backbone.Model.extend({
     defaults: function(){
       return {
         // Ostensibly immutable {
@@ -74,9 +18,15 @@ var v = {}, u = 0;
         // }
         
         // Modifiable {
-          previous: new ShapeGroup(),
-          next: new ShapeGroup(),
-          members: new ShapeGroup(),
+          // previous or next at same depth {
+            previous: new ShapeGroup(),
+            next: new ShapeGroup(),
+          // }
+
+          // relationship amongst depth {
+            container: new ShapeGroup(),
+            member: new ShapeGroup(),
+          // }
 
           label: "",
           content: "",
@@ -90,40 +40,44 @@ var v = {}, u = 0;
     initialize: function() {
       var mthis = this;
       this.uid = this.get('uid');
+      this.view = new v[this.get('type')](this);
 
       // Sets up the previous next add/remove hierarchy
       // and events
       _.each(['add', 'remove'], function(what) {
-        _.each(permute(['previous', 'next']), function(which) {
+        _.each(
+          [].concat(
+            permute(['container', 'member']),
+            permute(['previous', 'next'])
+          ), function(which) {
           mthis.get(which[0]).on(what, function(that) {
-            console.log([
-              that.get('type'), that.uid,
-              what, 
-              which[1],
-              mthis.get('type'), mthis.uid
-            ].join(' '));
             that.get(which[1])[what](mthis);
+            mthis.view.render();
           });
         });
       });
     },
 
-    walk: function() {
-      this.get('members').each(function(which) {
-        which.walk();
+    walk: function(what) {
+      var el = this.view.render().$el;
+      what.append(el);
+      this.get('member').each(function(which) {
+        which.walk(what);
       });
       this.get('next').each(function(which) {
-        which.walk();
+        which.walk(what);
       });
+      
+/*
       console.log(
         this.get('type'),
-        this.get('content'),
         this.uid,
         this.get('next').map(function(which) { return which.uid })
       );
+*/
     }
   });
-  _.each(['previous', 'next', 'member'], function(which) {
+  _.each(['previous', 'next', 'member', 'container'], function(which) {
     Shape.prototype[which] = function(what) {
       return what ? 
         this.get(which).add(what) :
@@ -131,8 +85,8 @@ var v = {}, u = 0;
     }
   });
 
-  _.each([ "Category", "Aside", "Description", "Intro", "Path", "Procedure" ], function(which) {
-    v[which] = Shape.extend({
+  _.each(NodeType, function(which) {
+    m[which] = Shape.extend({
       defaults: function(){
         return inheritAndAdd(Shape, {type: which});
       }
@@ -148,12 +102,14 @@ var v = {}, u = 0;
   }),
   DocGlobal = new ShapeGroup();
 
-  var root = new v.Category(),
-      A = new v.Intro(),
-      B = new v.Category();
+  var root = new m.Category({content: "Abraham Lincoln"}),
+      A = new m.Intro({content: "First Republican President"}),
+      B = new m.Category(),
+      C = new m.Description();
 
   root.next(A);
   A.next(B);
-  root.walk();
+  B.member(C);
+  root.walk($('#document'));
 
 })(jQuery);
